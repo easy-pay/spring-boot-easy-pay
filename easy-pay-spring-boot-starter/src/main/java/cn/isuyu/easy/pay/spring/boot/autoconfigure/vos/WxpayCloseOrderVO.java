071f7687c1d15ca204d130f98d875cebc960ad2f
niezhiliang <1132263176@qq.com>
2019-12-25 12:38:09 +0800

    v1.0.2

--- easy-pay-spring-boot-starter/src/main/java/cn/isuyu/easy/pay/spring/boot/autoconfigure/vos/WxpayCloseOrderVO.java
+++ easy-pay-spring-boot-starter/src/main/java/cn/isuyu/easy/pay/spring/boot/autoconfigure/vos/WxpayCloseOrderVO.java
@@ -3,8 +3,6 @@ package cn.isuyu.easy.pay.spring.boot.autoconfigure.vos;
 import com.github.binarywang.wxpay.bean.result.BaseWxPayResult;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-import java.io.Serializable;
-
 /**
  * @Author NieZhiLiang
  * @Email nzlsgg@163.com
